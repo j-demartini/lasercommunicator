2c255895efab381724c5abdf5927490ed603e06e
Joseph DeMartini <josephdemartini@gmail.com>
2025-05-26 13:49:09 -0400

    dwa

--- BaseCommunicator.cs
+++ BaseCommunicator.cs
@@ -40,7 +40,7 @@ public class BaseCommunicator
     public void ReceiveGPS()
     {
         Console.WriteLine("Attempting GPS receive...");
-        SerialPort port = new SerialPort("/dev/ttyS0", 19200);
+        SerialPort port = new SerialPort("/dev/ttyS0", 38400);
         port.Encoding = Encoding.ASCII;
         port.Open();
         if (port.IsOpen)
